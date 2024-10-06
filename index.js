cd60f194bc5be040af30a9663211bbc25834b589
Shantanu <shantanupathak01@gmail.com>
2024-10-06 16:16:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+
+/// array methods in js//
+// pop =>
+//myarray.pop()
+//push=>
+//myarray.push('hello')
+// shift
+ //myarray.shift()
+ //unshift
+//myarray.unshift('hello')
+//splice
+//myarray.splice(2,1)
+// myarray.splice(3,0,"hello")
+//console.log(myarray.length)
+//concat
+
+ //var a=  myarray.concat(mydata)
+  //includes
+       
+  //console.log(myarray.includes(2))
+
+  // find
+  
+ // var a=myarray.find(Element=>Element.email=="" && Element.passowrd.=="")
+
+
+   //hof
+   // map / foreach / reduce / filter =>arrow function
+   var myarray=[1,2,3,4,5]
+
+    let x= myarray.filter((x)=>{
+            return   x>2
+       })
+
+
+  console.log(x)
+
